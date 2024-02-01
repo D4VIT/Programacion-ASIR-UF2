ff8e2620f57811261ba946b5b21d4f18532f170a
Joan David <tjoan9067@gmail.com>
2024-02-01 10:01:32 +0100

    en mostrar menu creamos los apartados

--- pizzeria.py
+++ pizzeria.py
@@ -1,6 +1,11 @@
 pizzas_disponibles = []
 
 def mostrar_menu():
+    print("\n1. Tomar pedido")
+    print("2. Preparar pizza")
+    print("3. Gestionar Estoc")
+    print("4. Procesar Pago")
+    print("5. Salir")
 
 def tomar_pedido():
     print("Se esta haciendo un nuevo pedido")

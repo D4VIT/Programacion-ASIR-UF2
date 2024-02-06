7daae9e5eb775eff5aff75964909c62cdfe02fbf
Joan David <tjoan9067@gmail.com>
2024-02-06 11:03:10 +0100

    aqui creamos los siguientes apartados que tendra nuestro menu

--- observatori.py
+++ observatori.py
@@ -1,1 +1,7 @@
 def menu():
+    print("Benvingut al registre de temperatures")
+    print("--------------------------------------")
+    print("[RT] Registrar temperatures setmanals")
+    print("[MJ] Consultar temperatura mitjana")
+    print("[DF] Consultar diferencia maxima")
+    print("[FI] Sortir")

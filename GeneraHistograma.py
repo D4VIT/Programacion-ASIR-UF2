36d1badce0d9146ebc98f36a350ffc6b4b1c5796
Joan David <tjoan9067@gmail.com>
2024-02-19 12:09:29 +0100

    aqui hacemos una funcion para generar el histograma de tiradas

--- GeneraHistograma.py
+++ GeneraHistograma.py
@@ -2,3 +2,10 @@ def generar_tirada():
     seed = 12345
     seed = (1103515245 * seed + 12345) % (2**31 - 1)
     return (seed % 6) + 1 + (seed // 6) % 6 + 1
+
+def generar_histograma(num_tirades):
+    histograma = [0] * 11
+    for _ in range(num_tirades):
+        tirada = generar_tirada()
+        histograma[tirada - 2] += 1
+    return histograma

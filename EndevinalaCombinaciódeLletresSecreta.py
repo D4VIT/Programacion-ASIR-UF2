258d1aea5a717611f856155ae9e703c071f80f53
Joan David <tjoan9067@gmail.com>
2024-02-23 09:22:20 +0100

    aqui hacemos que la semilla y la lista de letras, seleccione las letras de la lista y forma una combinacion, la combinacion consistira en 5 letras en total

--- EndevinalaCombinaciódeLletresSecreta.py
+++ EndevinalaCombinaciódeLletresSecreta.py
@@ -2,3 +2,8 @@ def generar_combinacio_secreta():
     combinacio = ''
     lletres = 'abcdefghijklmnopqrstuvwxyz'
     seed = 12345
+    for _ in range(5):
+        index = seed % len(lletres)
+        combinacio += lletres[index]
+        seed //= 2
+    return combinacio

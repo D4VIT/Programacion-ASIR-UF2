201dcbf6e369409c88f742d7a9eaee92c458f7f2
Joan David <tjoan9067@gmail.com>
2024-02-29 08:38:27 +0100

    aqui creamos la funcion dividir con la funciona fun para el apartado dividir y despues introducimos las variables numero 1 y numero 2 y seguimos añadiendo la siguiente equacion

--- CalculadoraBàsicaenKotlin.kt
+++ CalculadoraBàsicaenKotlin.kt
@@ -9,3 +9,11 @@ fun restar(numero1: Int, numero2: Int): Int {
 fun multiplicar(numero1: Int, numero2: Int): Int {
     return numero1 * numero2
 }
+
+fun dividir(numero1: Int, numero2: Int): Int? {
+    return if (numero2 != 0) {
+        numero1 / numero2
+    } else {
+        null
+    }
+}

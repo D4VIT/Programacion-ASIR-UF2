07d69a8adaee884c03cdd4da3f9f114e4bca0f96
Joan David <tjoan9067@gmail.com>
2024-03-12 10:18:02 +0100

    ponemos la direccion izquierda

--- Modularitat_Classes.py
+++ Modularitat_Classes.py
@@ -4,3 +4,5 @@ def moure(coordenada, direccio):
     x, y = coordenada
     if direccio == 'dreta':
         nova_coordenada = (x + 1, y)
+    elif direccio == 'esquerra':
+        nova_coordenada = (x - 1, y)

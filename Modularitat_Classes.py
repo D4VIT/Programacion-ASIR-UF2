94010db7c190e10b4867924e71b03649ef7974ad
Joan David <tjoan9067@gmail.com>
2024-03-14 08:34:32 +0100

    aqui volvemos a rehacer el codigo y creamos las funciones de moverse a la derecha y moverse a la izquierda

--- Modularitat_Classes.py
+++ Modularitat_Classes.py
@@ -1,19 +1,11 @@
-coordenada = (0, 0)
+from Modularitat_Classes import coordenada
 
-def moure(coordenada, direccio):
+def moure_dreta(coordenada):
     x, y = coordenada
-    if direccio == 'dreta':
-        nova_coordenada = (x + 1, y)
-    elif direccio == 'esquerra':
-        nova_coordenada = (x - 1, y)
-    elif direccio == 'amunt':
-        nova_coordenada = (x, y + 1)
-    elif direccio == 'avall':
-        nova_coordenada = (x, y - 1)
+    nova_coordenada = (x + 1, y)
     return nova_coordenada
 
-coordenada = moure(coordenada, 'dreta')
-print(f"Nova coordenada despres de moure a la dreta: {coordenada}")
-
-coordenada = moure(coordenada, 'amunt')
-print(f"Nova coordenada despres de moure amunt: {coordenada}")
+def moure_esquerra(coordenada):
+    x, y = coordenada
+    nova_coordenada = (x - 1, y)
+    return nova_coordenada

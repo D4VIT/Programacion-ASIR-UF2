c03228e907af6e8cdc052d691eb26a0274b4ef71
Joan David <tjoan9067@gmail.com>
2024-03-14 08:38:05 +0100

    aqui creamos las funciones de moverse abajo y arriba

--- Modularitat_Classes.py
+++ Modularitat_Classes.py
@@ -1,4 +1,4 @@
-from Modularitat_Classes import coordenada
+import Modularitat_Classes
 
 def moure_dreta(coordenada):
     x, y = coordenada
@@ -8,4 +8,14 @@ def moure_dreta(coordenada):
 def moure_esquerra(coordenada):
     x, y = coordenada
     nova_coordenada = (x - 1, y)
+    return nova_coordenada
+
+def moure_amunt(coordenada):
+    x, y = coordenada
+    nova_coordenada = (x, y + 1)
+    return nova_coordenada
+
+def moure_avall(coordenada):
+    x, y = coordenada
+    nova_coordenada = (x, y - 1)
     return nova_coordenada

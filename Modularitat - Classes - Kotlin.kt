4dbe5d2f00efb80ab4a385fe40940c4ad573eec8
Joan David <tjoan9067@gmail.com>
2024-03-14 09:33:17 +0100

    aqui creamos las 5 funciones de moviemiento en kotlin

--- Modularitat - Classes - Kotlin.kt
+++ Modularitat - Classes - Kotlin.kt
@@ -1,1 +1,17 @@
+fun moureDreta(coordenada: Coordenada): Coordenada {
+    return Coordenada(coordenada.x + 1, coordenada.y)
+}
+
+fun moureEsquerra(coordenada: Coordenada): Coordenada {
+    return Coordenada(coordenada.x - 1, coordenada.y)
+}
+
+fun moureAmunt(coordenada: Coordenada): Coordenada {
+    return Coordenada(coordenada.x, coordenada.y + 1)
+}
+
+fun moureAvall(coordenada: Coordenada): Coordenada {
+    return Coordenada(coordenada.x, coordenada.y - 1)
+}
+
 

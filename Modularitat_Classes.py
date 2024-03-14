7fa0335eb7050635ffd6815cc9b8d0a87fe9d3ce
Joan David <tjoan9067@gmail.com>
2024-03-14 08:43:57 +0100

    aqui creamos los ejecutables de los movimientos

--- Modularitat_Classes.py
+++ Modularitat_Classes.py
@@ -19,3 +19,13 @@ def moure_avall(coordenada):
     x, y = coordenada
     nova_coordenada = (x, y - 1)
     return nova_coordenada
+
+def executar_moviment(coordenada, moviment):
+    if moviment == "dreta":
+        return moure_dreta(coordenada)
+    elif moviment == "esquerra":
+        return moure_esquerra(coordenada)
+    elif moviment == "amunt":
+        return moure_amunt(coordenada)
+    elif moviment == "avall":
+        return moure_avall(coordenada)

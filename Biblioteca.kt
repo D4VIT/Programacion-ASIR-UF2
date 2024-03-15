fe0af0af26d2ffdbf73992540992d34ece61e809
Joan David <tjoan9067@gmail.com>
2024-03-15 08:45:43 +0100

    creamos los apartados de libro, socios y los prestamos

--- Biblioteca.kt
+++ Biblioteca.kt
@@ -1,1 +1,11 @@
 
+fun main() {
+    val libro1 = Libro("El Señor de los Anillos", "J.R.R. Tolkien", 2)
+    val libro2 = Libro("Harry Potter", "J.K. Rowling", 3)
+
+    val socio1 = Socio("Juan", "Perez", 1)
+    val socio2 = Socio("María", "García", 2)
+
+    val prestamo1 = Prestamo(libro1, socio1, "01/10/2021")
+    val prestamo2 = Prestamo(libro2, socio2, "02/10/2021")
+}

aaa04bc43588fa36cf9008bf2c4d4184779fb735
Joan David <tjoan9067@gmail.com>
2024-04-04 10:01:41 +0200

    top down

--- Eliminar carpetes i contingut.py
+++ Eliminar carpetes i contingut.py
@@ -1,1 +1,9 @@
-
+'''
+1.Obtener la ruta de la carpeta
+1.1 leer la ruta de la carpeta
+1.2 verificar que la ruta es correcta
+1.3 Verificar que es una carpeta
+2.Eliminar contenido de la carpeta
+2.1Eliminar la carpeta
+2.1.2 verificar que esta eliminada correctamente
+'''

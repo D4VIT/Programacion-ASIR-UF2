edc38accd84583a67b02dfcbd07697c2b4f7f36d
Joan David <tjoan9067@gmail.com>
2024-05-14 10:39:01 +0200

    creamos la clase base para representar un libro

--- Gestio_llibres.java
+++ Gestio_llibres.java
@@ -1,1 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
 
+class Llibre {
+    private String titol;
+
+    public Llibre(String titol) {
+        this.titol = titol;
+    }
+
+    public String getTitol() {
+        return titol;
+    }
+}
